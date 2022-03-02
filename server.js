5b6cacdf5934391c451bc2d00ca4f6ab08fd6dd6
CrisMer Quinde <cmquinde@espol.edu.ec>
2022-03-02 17:42:03 +0000

    funcionalidad

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+
 const cors = require('cors')
-require('dotenv').config()
+
+const mongoose = require('mongoose')
+let uri ='mongodb+srv://movildesa:414vapHh1kdg9ACw@cluster0.ui7nr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.use(cors())
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -11,8 +19,125 @@ app.get('/', (req, res) => {
 
 
 
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
+let exerciseSessionSchema = new mongoose.Schema({
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: String
+})
+
+let userSchema = new mongoose.Schema({
+  username: {type: String, required: true},
+  log: [exerciseSessionSchema]
+})
+
+let Session = mongoose.model('Session', exerciseSessionSchema)
+let User = mongoose.model('User', userSchema)
+
+app.post('/api/users', bodyParser.urlencoded({ extended: false }), (request, response) => {
+  let newUser = new User({username: request.body.username})
+  newUser.save((error, savedUser) => {
+    if(!error){
+      let responseObject = {}
+      responseObject['username'] = savedUser.username
+      responseObject['_id'] = savedUser.id
+      response.json(responseObject)
+    }
+  })
+})
+
+app.get('/api/users', (request, response) => {
+  
+  User.find({}, (error, arrayOfUsers) => {
+    if(!error){
+      response.json(arrayOfUsers)
+    }
+  })
+  
+})
+
+app.post('/api/users/:_id/exercises', bodyParser.urlencoded({ extended: true }) , (request, response) => {
+  
+  let newSession = new Session({
+    description: request.body.description,
+    duration: parseInt(request.body.duration),
+    date: request.body.date
+  })
+  
+  if(newSession.date === ''){
+    newSession.date = new Date().toISOString().substring(0, 10)
+  }
+  
+  User.findByIdAndUpdate(
+    request.params._id,
+    {$push : {log: newSession}},
+    {new: true},
+    (error, updatedUser)=> {
+      if(!error){
+        let responseObject = {}
+        responseObject['_id'] = updatedUser.id
+        responseObject['username'] = updatedUser.username
+        responseObject['date'] = new Date(newSession.date).toDateString()
+        responseObject['description'] = newSession.description
+        responseObject['duration'] = newSession.duration
+        response.json(responseObject)
+      }
+    }
+  )
+})
+
+app.get('/api/users/:_id/logs', (request, response) => {
+  console.log(request.params._id)
+  User.findById(request.params._id, (error, result) => {
+    if(!error){
+      let responseObject = result
+      
+      if(request.query.from || request.query.to){
+        
+        let fromDate = new Date(0)
+        let toDate = new Date()
+        
+        if(request.query.from){
+          fromDate = new Date(request.query.from)
+        }
+        
+        if(request.query.to){
+          toDate = new Date(request.query.to)
+        }
+        
+        fromDate = fromDate.getTime()
+        toDate = toDate.getTime()
+        
+        responseObject.log = responseObject.log.filter((session) => {
+          let sessionDate = new Date(session.date).getTime() 
+          return sessionDate>= fromDate && sessionDate <= toDate 
+        }) 
+      }
+      
+      if(request.query.limit){
+        responseObject.log = responseObject.log.slice(0, request.query.limit)
+        
+      }
+     
+      responseObject = responseObject.toJSON()
+      responseObject['count'] = result.log.length
+   
+      
+      for (i=0; i>=result.log.length; i++){
+        responseObject.log[i].date = new 
+ Date(responseObject.log[i].date).toDateString();
+      }
+      
+ 
+      response.json(responseObject)
+    }
+  })
+  
+})
+
+
+ 
